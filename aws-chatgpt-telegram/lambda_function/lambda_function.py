c01ca540e145a8570831b153a01f49d92483e1fe
Devan <devan@cloudandthings.io>
2023-05-23 20:43:19 +0200

    Fix: Adding prints

--- aws-chatgpt-telegram/lambda_function/lambda_function.py
+++ aws-chatgpt-telegram/lambda_function/lambda_function.py
@@ -87,6 +87,8 @@ def process_message(update, context):
 ############################
 
 def message_handler(event, context):
+    print(json.loads(event["headers"]))
+    print(json.loads(event))
     if authenticate_secret_token(json.loads(event["headers"]["X-Telegram-Bot-Api-Secret-Token"])):
         dispatcher.add_handler(MessageHandler(Filters.text, process_message))
         dispatcher.add_handler(MessageHandler(Filters.voice, process_voice_message))
